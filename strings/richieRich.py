cecdc765e82894eb9ce59794ae1c2bb148864024
lromang <luis.roangarci@gmail.com>
2017-05-06 17:56:27 -0500

    working

--- strings/richieRich.py
+++ strings/richieRich.py
@@ -17,43 +17,49 @@ def isPal(num):
 
 def palNum(num, k):
     mismatch = toPal(num)
+    changed  = []
     if len(mismatch) > k:
         return -1
     ## Advantage, you can take the greedy step!
-    ## only need to focus on the first index of mismatch!
+    ## only need to focus on the first index of mismatch
+    ## per iteration!
     palIndex = 0
     while k > 0:
-        ## If we can change both digits.
-        ## and neither of does is 9, make two replacments for 9
+        ## If there are mismatches left
         if len(mismatch) > 0:
-            if (k - 1) >= len(mismatch) and \
-               (num[mismatch[0]] != 9 and num[len(num) - 1 - mismatch[0]] !=9):
-                num[mismatch[0]] = 9
-                num[len(num) - 1 - mismatch[0]] = 9
-                mismatch.pop(0)
-                k = k - 2
-            else:
+            lindex = mismatch[0]
+            rindex = len(num) - (1 + mismatch[0])
+            ## If we can change both digits.
+            ## and neither of thoes is 9, make two replacments for 9
+            ## k-1 because we are removing a mismatch
+            ##if ((k - 1) >= len(mismatch)) and (num[lindex] != 9 and num[rindex] != 9):
+            ##    num[lindex] = 9
+            ##    num[rindex] = 9
+            ##    mismatch.pop(0)
+            ##    k = k - 2
+            ##else:
                 ## If we only can make one change or
                 ## one of the digits is 9, make one replacement
                 ## for the largest possible number
-                replace = mismatch[0] if num[mismatch[0]] <= \
-                          num[len(num) - 1 - mismatch[0]] else  len(num) - 1 - mismatch[0]
-                num[replace] = max(num[mismatch[0]], \
-                                   num[len(num) - 1 - mismatch[0]])
-                mismatch.pop(0)
-                k = k - 1
+            replace      = lindex if num[lindex] <= \
+                           num[rindex] else rindex
+            num[replace] = max(num[lindex], num[rindex])
+            changed.append(mismatch.pop(0))
+            k = k - 1
         else:
             if palIndex > (len(num) / 2):
                 return num
-            if k >= 2 and num[palIndex] != 9:
+            if (len(num) % 2 == 1) and (palIndex == (len(num) / 2)) and k == 1:
+                num[palIndex] = 9
+                return num
+            if k >= 2 and num[palIndex] != 9 and palIndex not in changed:
                 num[palIndex] = 9
                 num[len(num) - 1 - palIndex] = 9
                 k = k - 2
-                palIndex = palIndex + 1
-            if (len(num) % 2 == 1) and (palIndex == (len(num)/2)) and k == 1:
+            elif k >= 1 and num[palIndex] != 9 and palIndex in changed:
                 num[palIndex] = 9
+                num[len(num) - 1 - palIndex] = 9
                 k = k - 1
-                palIndex = palIndex + 1
             palIndex = palIndex + 1
     return num
 
